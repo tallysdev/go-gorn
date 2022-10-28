7ca7c3cce7317244b565d62d29f1248869559e56
Tallys Aureliano <91434644+Tallys-Aureliano@users.noreply.github.com>
2022-10-28 11:24:28 -0300

    teste

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
-// You can edit this code!
 // Click here and start typing.
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, 世界")
+	handler := http.HandlerFunc(hello)
+	http.ListenAndServe(".8080", handler)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 }
